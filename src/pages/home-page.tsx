95621528b8d36a2fa26f7e6d0a763e99eb09d9de
Christian Klein <xcklein@gmail.com>
2025-12-26 13:58:58 -0600

    avatar on home page

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,4 +1,5 @@
 import { Bubbles } from "@/components/bubbles";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Rain } from "../components/rain";
 
 export function HomePage() {
@@ -6,7 +7,10 @@ export function HomePage() {
     <>
       <div className="m-auto flex max-w-2xl flex-col items-center justify-center gap-4 p-4">
         <Rain aria-hidden="true" className="pointer-events-none absolute -z-10 h-full w-full" />
-        <img src="images/christian-circle.png" alt="Avatar of Christian" className="w-96" />
+        <Avatar className="size-96">
+          <AvatarImage src="/images/christian-circle.png" />
+          <AvatarFallback className="text-4xl">C</AvatarFallback>
+        </Avatar>
         <h1 className="text-4xl font-bold">Christian</h1>
         <Bubbles className="pointer-events-none absolute h-full w-full" />
       </div>
