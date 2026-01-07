68e7bc372779c4d7180ec916726a331c4f956dbf
Christian Klein <xcklein@gmail.com>
2026-01-06 21:17:36 -0600

    fix tests

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,32 @@
+/// <reference lib="dom" />
+
 import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
-import { afterEach } from "vitest";
+import { afterEach, vi } from "vitest";
+
+// Polyfill pointer capture for Drawer component testing (vaul library)
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+if (!HTMLElement.prototype.setPointerCapture) {
+  HTMLElement.prototype.setPointerCapture = () => {};
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+if (!HTMLElement.prototype.releasePointerCapture) {
+  HTMLElement.prototype.releasePointerCapture = () => {};
+}
+
+// Polyfill getComputedStyle to handle transform property
+const originalGetComputedStyle = window.getComputedStyle;
+vi.stubGlobal("getComputedStyle", (element: Element, pseudoElt?: string | null) => {
+  const computedStyle = originalGetComputedStyle(element, pseudoElt);
+  if (!computedStyle.transform) {
+    Object.defineProperty(computedStyle, "transform", {
+      value: "none",
+      writable: true,
+    });
+  }
+  return computedStyle;
+});
 
 afterEach(() => {
   cleanup();
